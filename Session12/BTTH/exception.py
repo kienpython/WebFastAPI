263a753bf445ac6003275b4ccde31addf65220c9
kienpython <129153039+kienpython@users.noreply.github.com>
2026-07-09 09:07:45 +0700

    Update exception.py

--- Session12/BTTH/exception.py
+++ Session12/BTTH/exception.py
@@ -1,17 +1,18 @@
+# Chứa khởi tạo @app.exception_handle
+
 from fastapi.exceptions import RequestValidationError
+from fastapi import Request, HTTPException, status, FastAPI
 from fastapi.responses import JSONResponse
-from fastapi import Request, HTTPException, status
-from main import app
-from service import create_response
+from services import create_response
+def exception(app):
+    @app.exception_handler(HTTPException)
+    def http_exception_handle(request: Request, exc:HTTPException):
+        return create_response(status_code=exc.status_code, errors=exc.detail)
 
-@app.exception_handler(RequestValidationError)
-def validation_exception_handle(request: Request, exc: RequestValidationError):
-    return create_response(status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, message="Data không hợp lệ!", path=request.url.path)
+    @app.exception_handler(RequestValidationError)
+    def validation_exception_handle(request: Request, exc:RequestValidationError):
+        return create_response(status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, errors="Dữ liệu không hợp lệ!")
 
-@app.exception_handler(HTTPException)
-def http_exception_handle(request: Request, exc: HTTPException):
-    return create_response(status_code=exc.status_code, errors=exc.detail, path=request.url.path)
-
-@app.exception_handler(Exception)
-def exception_handle(request: Request, exc: Exception):
-    return create_response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, errors="Lỗi hệ thống!", path=request.url.path)
+    @app.exception_handler(Exception)
+    def exception_handle(request: Request, exc:Exception):
+        return create_response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, errors="Lỗi hệ thống!")
